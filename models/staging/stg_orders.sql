5708f78d9d4271e220ebdaf52f6ec7146105eaed
宇軒 邱 <arcade0425@gmail.com>
2025-09-14 07:50:11 +0000

    feat: add order_status_mapping from seed

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,6 +1,24 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
-from {{ source("jaffle_shop", "orders")}}
+with source as (
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+    from {{ source("jaffle_shop", "orders")}}
+),
+
+order_status_mapping as (select * from {{ ref('seed_order_statuses') }}),
+
+transformed as (
+    select
+        s.order_id,
+        s.customer_id,
+        s.order_date,
+        s.status,
+        osm.is_valid
+    from source as s
+    left join order_status_mapping as osm on s.status = osm.status
+
+)
+
+select * from transformed
